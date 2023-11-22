5520b6b3f40ee1b0bec60079037babacd6aa5434
Makarenkov Aleksandr <aarenaarenson@gmail.com>
2023-11-22 13:46:55 +0300

    majority-element

---
+++ majority-element/majority-element.go
@@ -0,0 +1,18 @@
+package majority_element
+
+func majorityElement(nums []int) int {
+	m := 0
+	val := 0
+	counts := map[int]int{}
+	for i := 0; i < len(nums); i++ {
+		counts[nums[i]]++
+	}
+	for k, v := range counts {
+		if v > m {
+			val = k
+			m = v
+		}
+	}
+
+	return val
+}
